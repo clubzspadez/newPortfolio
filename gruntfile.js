d1a99629f78b25382590970c8c7dd66d75e373c3
Jonathan Rodriguez <clubzspadez@gmail.com>
2017-08-14 20:17:54 -0400

    finished portfolio v1

---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+// grunt.loadNpmTasks('grunt-contrib-cssmin');
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            name: 'small',
+            width: '30%',
+            suffix: '_small',
+            quality: 20
+          },{
+            name: 'large',
+            width: '50%',
+            suffix: '_large',
+            quality: 40
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images/',
+          dest: 'images_src/'
+        }]
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
+
+};
